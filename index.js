0dce52ea9012a94f34c0c54202679703bcfe3b39
tusharbisht16 <tusharbisht0216@gmail.com>
2024-03-28 16:12:33 +0530

    local api

---
+++ index.js
@@ -0,0 +1,16 @@
+// let container = document.getElementById("container");
+
+
+
+// async function fetchData(){
+// try{
+// let res = await fetch("http://localhost:3000/data");
+// let dat= await res.json();
+// console.log(dat)
+// }
+// catch(err){
+//     console.log(err)
+// }
+// }
+
+// fetchData();
